75f7c4dde52569ba1f855f8534f53463bdc26591
Jiří Špác <jiri.spac.external@socialbakers.com>
2015-09-28 13:48:20 +0200

    added first two specs

---
+++ test/nexridge.spec.js
@@ -0,0 +1,10 @@
+/* global describe, it */
+describe('nexridge', function () {
+  it('should create a server which listens on socket.io for any query', function (done) {
+
+  })
+
+  it('should make the query into our test DB and return the results', function () {
+
+  })
+})

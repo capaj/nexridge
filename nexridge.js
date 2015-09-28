900b0f3d66444f34d660512ca4bbc0f4072b36d4
Jiří Špác <jiri.spac.external@socialbakers.com>
2015-09-28 21:17:57 +0200

    first server implementation fixed a bit-passed a manual test with simple select

--- nexridge.js
+++ nexridge.js
@@ -3,15 +3,21 @@ const RPC = require('socket.io-rpc')
 const debug = require('debug')('nexridge:server')
 const express = require('express')
 const knex = require('knex')
+const _ = require('lodash')
+let db
 
-function listen() {
-  var rpcApp = RPC.apply(RPC, arguments)
-  rpcApp.expose({
-    query: function query(JSON){
-      let q = knex.queryBuilder()
-      _.merge(q, JSON)
-      return q
-    }
-  })
+module.exports = {
+  listen: function listen() {
+    var rpcApp = RPC.apply(RPC, arguments)
+    rpcApp.expose({
+      query: function query(JSON){
+        let q = db.queryBuilder()
+        _.merge(q, JSON)
+        return q
+      }
+    })
+  },
+  connect: (config) => {
+    db = knex(config)
+  }
 }
-module.exports = listen
--- test/test-server.js
+++ test/test-server.js
@@ -1,3 +1,12 @@
 var nexridge = require('../nexridge')
 
-nexridge(8197)
+nexridge.connect({
+  client: 'mysql2',
+  connection: {
+    host     : '127.0.0.1',
+    user     : 'travis',
+    password : '',
+    database : 'test_db'
+  }
+})
+nexridge.listen(8197)
